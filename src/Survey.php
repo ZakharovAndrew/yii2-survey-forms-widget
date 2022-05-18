0e7c998f19242db39f90f84fa8e1a0296278dfdb
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-05-18 10:14:18 +0300

    Create Survey.php

---
+++ src/Survey.php
@@ -0,0 +1,17 @@
+namespace app\components;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class Survey extends Widget {
+  public function init()
+      {
+          parent::init();
+      }
+
+      public function run()
+      {
+          return 'Test';
+      }
+  }
+}
