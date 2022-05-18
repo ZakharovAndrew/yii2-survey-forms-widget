b2a027d2840537e792f3821603d43c3b5cebe951
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-05-18 11:12:22 +0300

    Update SyrveyAsset.php

--- src/SyrveyAsset.php
+++ src/SyrveyAsset.php
@@ -5,8 +5,7 @@ use yii\web\AssetBundle;
 
 class SyrveyAsset extends AssetBundle
 {
-    public function init()
-    {
-        parent::init();
-    }
+    public $sourcePath = '@vendor/zakharov-andrew/survey-forms/';
+    public $css = ["css/SurveyForms.css"];
+    public $js = ["src/SurveyForms.js"];
 }
